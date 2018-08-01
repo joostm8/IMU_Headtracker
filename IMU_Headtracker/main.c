6fb6544218351115d88aed5a15134a34c6d00789
Joost Mertens <joost.mrtns@gmail.com>
2018-08-01 22:07:43 +0200

    Added comments concerning which code to place where

--- IMU_Headtracker/main.c
+++ IMU_Headtracker/main.c
@@ -3,16 +3,51 @@
  *
  * Created: 1/08/2018 20:43:46
  * Author : Joost
+ *
+ * IMU headtracker
+ * Requires:
+ * MPU9250
+ * Atmega328p
+ * rn42 (or hc05 flashed as rn42)
+ * 
  */ 
 
 #include <avr/io.h>
+#include "TWI_Master.h"
 
+static enum programState{
+	startup,
+	calibration,
+	operation
+	} pState = startup;
+	
+static enum bluetoothState{
+	startup,
+	ready,
+	connected
+	} btState = startup;
 
 int main(void)
 {
     /* Replace with your application code */
+	
+	/* Add startup code here*/
+	
+	// Init microcontroller
+	
+	// Init MPU9250
+	
+	// Init Bluetooth controller (if needed)
+	
+	/* Add calibration code here */
+	
+	// Calibrate magneto
+	
+	// Calibrate gyro
+	
     while (1) 
     {
+		/* Continuously update Bluetooth controller with axes info*/
     }
 }
 
