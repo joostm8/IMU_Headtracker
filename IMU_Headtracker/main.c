8180733d099da7653211b13be30024f533fa9d24
Joost Mertens <joost.mrtns@gmail.com>
2018-08-01 20:46:08 +0200

    Initial commit

---
+++ IMU_Headtracker/main.c
@@ -0,0 +1,18 @@
+/*
+ * IMU_Headtracker.c
+ *
+ * Created: 1/08/2018 20:43:46
+ * Author : Joost
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
